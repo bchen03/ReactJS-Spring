572d73e7a552d2da464c14c159895bc73edb48cf
bchen03 <benny.chen@groupm.com>
2018-01-25 17:08:09 -0500

    Added API Host input field to NavBar. Upgraded WebPack version and added Webpack Dev Server support. Changes to facilitate hot-reloading of app.js changes.

--- basic/src/main/js/app.js
+++ basic/src/main/js/app.js
@@ -2,10 +2,9 @@
 
 const React = require('react');
 const ReactDOM = require('react-dom');
-//const client = require('./client');
+var PropTypes = require('prop-types');
 
 class App extends React.Component {
-
 	constructor(props) {
 		super(props);
 
@@ -14,11 +13,13 @@ class App extends React.Component {
 		this.state = {
 		    files: [],
 		    host: window.location.href,
+		    //host: "http://localhost:8080/",
 		    errorMessage: "",
 		    isLoading: false
         };
 
 		this.reloadFiles = this.reloadFiles.bind(this);
+		this.handleChange = this.handleChange.bind(this);
 	}
 
 	componentDidMount() {
@@ -53,15 +54,20 @@ class App extends React.Component {
 
     renderResult() {
         // Show a loading panel if waiting for results
-        return (!this.state.isLoading) ?
-            <FileList files={this.state.files} /> :
-            <div className="container mt-3">Loading data. Please wait...</div>
+//        return (!this.state.isLoading) ?
+//            <FileList files={this.state.files} /> :
+//            <div className="container mt-3">Loading data. Please wait...</div>
+    }
+
+    handleChange(event) {
+        console.log("App:handleChange, event: ", event.target.value);
+        this.setState({host: event.target.value});
     }
 
 	render() {
 		return (
 		    <div style={{height:"100%"}}>
-                <NavBar host={this.state.host} onReload={this.reloadFiles} />
+                <NavBar host={this.state.host} onChange={this.handleChange} onReload={this.reloadFiles} />
                 <ErrorPanel errorMessage={this.state.errorMessage} />
 
                 {/*HOC:*/}
@@ -82,7 +88,7 @@ class NavBar extends React.Component {
     render() {
         return (
             <nav className="navbar navbar-expand-lg navbar-dark mdb-color darken-1 container-fluid">
-                <a className="navbar-brand" href="#">mPlatform QA Tests</a>
+                <a className="navbar-brand pt-3" href="#"><h3><strong>mPlatform File Viewer</strong></h3></a>
 
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
@@ -91,15 +97,24 @@ class NavBar extends React.Component {
                     <ul className="navbar-nav mr-auto">
                     </ul>
 
+                    <form className="form-inline">
+                        <input className="form-control mr-4" type="text"
+                            onChange={this.props.onChange} value={this.props.host} placeholder="API Host" aria-label="API Host" />
+                    </form>
+
                     <ul className="navbar-nav">
                         <li className="nav-item">
-        		            <button type="button" className="btn btn-info" onClick={this.props.onReload}>Reload</button>
+        		            <button type="button" className="btn warning-color" onClick={this.props.onReload}>Reload</button>
                         </li>
                     </ul>
                 </div>
             </nav>
         );
     }
+}
+
+NavBar.propTypes = {
+    onReload: PropTypes.func
 }
 
 function ErrorPanel(props) {
@@ -113,6 +128,10 @@ function ErrorPanel(props) {
         return <div style={errorStyle}>{props.errorMessage}</div>;
     else
         return <div></div>
+}
+
+ErrorPanel.propTypes = {
+    errorMessage: PropTypes.string
 }
 
 class ReloadButton extends React.Component {
@@ -150,6 +169,10 @@ class FileList extends React.Component {
             </div>
         );
     }
+}
+
+FileList.propTypes = {
+    files: PropTypes.arrayOf(PropTypes.object)
 }
 
 // Higher Order Component (HOC) - Basically the Decorator pattern
@@ -195,11 +218,18 @@ class File extends React.Component {
     render() {
         return (
             <a className="list-group-item list-group-item-action" href={this.props.file.url} target="_blank">
-                <i className="fa fa-file-text-o mr-1" aria-hidden="true"></i>
-                <span>{this.props.file.title}</span>
+                <i className="fa fa-file-text-o" aria-hidden="true"></i>
+                <span className="ml-2">{this.props.file.title}</span>
             </a>
         );
     }
+}
+
+File.propTypes = {
+    file: PropTypes.shape({
+        title: PropTypes.string,
+        url: PropTypes.string
+    })
 }
 
 class Footer extends React.Component {
@@ -210,7 +240,7 @@ class Footer extends React.Component {
                 <footer className="page-footer navbar-dark mdb-color darken-1 center-on-small-only pt-0 container-fluid fixed-bottom">
                     <div className="footer-copyright row">
                         <div className="container-fluid">
-                            © 2018 Copyright: <a href="http://localhost:8080">mPlatform QA Tests</a>
+                            © 2018 Copyright: <a href="http://localhost:8080">mPlatform File Viewer</a>
                         </div>
                     </div>
                 </footer>
